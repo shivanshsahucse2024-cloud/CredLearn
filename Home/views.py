4a758845e59502c0ccf86759f3f0bf57cfdb52ee
shivanshsahucse2024-cloud <shivansh.sahucse2024@indoreinstitue.com>
2025-12-04 18:39:54 +0530

    Initial complete project push

---
+++ Home/views.py
@@ -0,0 +1,61 @@
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from Home.models import Profile, Video, VideoView
+
+# Create your views here.
+#def index(request):
+    #return HttpResponse( 'This is homepage of Teach and Earn platform' )
+    
+def home(request):
+    return render(request,'index.html')
+
+def Explore(request):
+    return render( request,'explore.html' )
+
+def about(request):
+    return render(request,'about.html' )
+
+def contact(request):
+    return render(request,'contact.html' )
+
+def services(request):
+    return render(request,'services.html' )
+
+def programming(request):
+    return render(request,'programming.html')
+
+def Design(request):
+    return render(request,'Design.html')
+
+def Music(request):
+    return render(request,'music.html')
+
+def Lang(request):
+    return render(request,'Lang.html')
+
+def mentor(request):
+    return render(request,'mentor.html')
+
+@login_required
+def wallet(request):
+    profile = request.user.profile
+
+    # Credits earned (from uploads)
+    uploads = Video.objects.filter(owner=request.user)
+
+    # Credits spent (from watching others)
+    spent = VideoView.objects.filter(viewer=request.user).exclude(video__owner=request.user)
+
+    context = {
+        "profile": profile,
+        "uploads": uploads,
+        "spent": spent
+    }
+    return render(request, "wallet.html", context)
+
+def wallet(request):
+    user = request.user
+
+    profile, created = Profile.objects.get_or_create(user=user)
+
+    return render(request, 'wallet.html', {'profile': profile})
